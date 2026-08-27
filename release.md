3db45bbcc5ad2a3876210014cab9402d41550d31
Ruan Faustino Camello Da Silva <ruanfaustinocamellodasilva@gmail.com>
2026-08-27 15:03:36 -0300

    feat: add release script and versioning support with app icon

---
+++ release.md
@@ -0,0 +1,8 @@
+# Initial release
+
+This release improves form-filling reliability and performance across supported sites.
+It adds more robust Excel data extraction and error handling to reduce crashes.
+Multi-language support (including Portuguese refinements) has been expanded.
+Chromedriver and browser-automation components were updated for better compatibility.
+Logging, temporary-file cleanup, and minor bug fixes improve stability and diagnostics.
+Overall, this release focuses on reliability, compatibility, and maintainability.
---
+++ release.sh
@@ -0,0 +1,41 @@
+#!/bin/env bash
+
+set -e
+
+if [[ -z "$VIRTUAL_ENV" ]]; then
+	python -m venv .venv
+    source .venv/Scripts/activate
+fi
+
+git fetch --tags # Fetch git tags and get the latest one
+version=$(git describe --tags $(git rev-list --tags --max-count=1))
+version_number="${version#v}"  # Remove 'v' prefix if the tag has it
+
+# Extract major, minor, and patch versions
+IFS='.' read -r MAJOR_VERSION MINOR_VERSION PATCH_VERSION <<< "$version_number"
+export MAJOR_VERSION MINOR_VERSION PATCH_VERSION
+
+# Write version file
+envsubst < version.txt > version_file.tmp
+mv version_file.tmp version.txt
+cat version.txt
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Build executable with pyinstaller
+pyinstaller -n lnc_bot --clean --icon appicon.ico --version-file version.txt --paths src src/lnc_bot.py
+
+# Restore files with sensible data
+cp src/lnc_bot.ini src/lnc_bot.ini.bak
+git restore src/lnc_bot.ini
+
+# Compress executable and related files
+zip -j lnc_bot.zip dist/lnc_bot.exe src/lnc_bot.ini src/path.ini
+cd src ; zip -r ../lnc_bot.zip Resources/*.lang ; cd ..
+
+# Create a release on GitHub
+gh release create $version --verify-tag --notes-file release.md --title "lnc_bot ${version} release" lnc_bot.zip
+
+# Reverting placeholder files
+git restore version.txt
